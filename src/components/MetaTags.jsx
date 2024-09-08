afd265dede91ae7f9b8b868ddeedc68610b272d9
Alessandro Chrystian <alessandroc.dev@gmail.com>
2024-09-08 02:38:01 -0300

    sociallogo

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import socialLogo from '../assets/images/social-logo.png'
 
-const socialLogoUrl = 'https://www.aledev.com.br/assets/social-logo-inS7Ky25.png'
+const socialLogoUrl = 'https://www.aledev.com.br/assets/social-logo.png'
 
 const MetaTags = () => {
   return (
